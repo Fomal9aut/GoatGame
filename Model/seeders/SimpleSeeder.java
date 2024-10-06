2836b25115388a48a1793844bd7a893f46517247
Fomal9aut <sergeymarchenko903@gmail.com>
2024-10-06 22:16:34 +0300

    docs updated

--- Model/seeders/SimpleSeeder.java
+++ Model/seeders/SimpleSeeder.java
@@ -11,8 +11,8 @@ import Model.units.Wall;
 public class SimpleSeeder extends Seeder {
 
     @Override
-    protected void seedRobot() {
-        _field.cell(4, 0).putUnit( _field.Goat() );
+    protected void seedGoat() {
+        _field.cell(0, 0).putUnit( _field.Goat() );
     }
 
     @Override
@@ -21,23 +21,23 @@ public class SimpleSeeder extends Seeder {
     @Override
     protected void seedUnits() {
 
-        Cell underRobotCell = _field.Goat().typedOwner();
-        underRobotCell = underRobotCell.neighbour( Direction.south()) ;
+        Cell underGoatCell = _field.Goat().typedOwner();
+        underGoatCell = underGoatCell.neighbour( Direction.south()) ;
         _field.cell(2, 2).putUnit( new Box() );
-        underRobotCell.putUnit( new Box());
-        _field.cell(4, 4).putUnit(new Key());
-        _field.cell(5, 5).putUnit(new Key());
+        underGoatCell.putUnit( new Box());
+        //_field.cell(4, 4).putUnit(new Key());
+        //_field.cell(5, 5).putUnit(new Key());
 
         for(int i = 0; i < _field.height(); i++)
-            _field.cell(i,6).putUnit(new Wall());
+            _field.cell(i,9 - i).putUnit(new Wall());
 
-        Teleporter tp1= new Teleporter();
-        Teleporter tp2= new Teleporter();
-
-        _field.cell(0,0).putUnit(tp1);
-        _field.cell(0,8).putUnit(tp2);
-
-        tp1.Connect(tp2);
+//        Teleporter tp1= new Teleporter();
+//        Teleporter tp2= new Teleporter();
+//
+//        _field.cell(0,2).putUnit(tp1);
+//        _field.cell(8,8).putUnit(tp2);
+//
+//        tp1.Connect(tp2);
 
     }
 }
